8a425763449478db0ba036fea13369bbc9d9ef56
MRMSANJAI <mrmsanjai@gmail.com>
2025-01-30 08:31:06 +0530

    300125

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Sidebar from './Components/Sidebar'
+import Player from './Components/Player'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+       <Sidebar/>
+      </div>
+      <Player/>
+    </div>
   )
 }
 
