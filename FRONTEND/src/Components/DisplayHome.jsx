a1eb773a8fb3f2b117b212c047e59767645c6bf9
MRMSANJAI <mrmsanjai@gmail.com>
2025-03-18 21:44:21 +0530

    180325

--- FRONTEND/src/Components/DisplayHome.jsx
+++ FRONTEND/src/Components/DisplayHome.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import Navbar from './Navbar'
 import { albumsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
+import { songsData } from '../assets/assets'
+import Songitem from './Songitem'
 
 const DisplayHome = () => {
   return (
 <>
 <Navbar/>
-<div className=' my-5 font-boldb text-2xl'>
-    {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+<div className=' mb-4 '>
+  <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+  <div className='flex overflow-auto'>
+  {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+  </div>
+</div>
+<div className=' mb-4 '>
+  <h1 className='my-5 font-bold text-2xl'>Todays Hits</h1>
+  <div className='flex overflow-auto'>
+ {songsData.map((item,index)=>(<Songitem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+  </div>
 </div>
 </>
   )
