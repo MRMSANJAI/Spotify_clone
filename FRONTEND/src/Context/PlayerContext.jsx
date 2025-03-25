4173ab55f7f74f1017ef2901146d3db83c5e295e
MRMSANJAI <mrmsanjai@gmail.com>
2025-03-25 20:41:18 +0530

    250325

--- FRONTEND/src/Context/PlayerContext.jsx
+++ FRONTEND/src/Context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
@@ -31,7 +31,31 @@ const PlayerContextProvider = (props)=>{
         audioRef.current.pause();
         setPlaying(false);
     }
- 
+
+    const playWithId = async(id)=>{
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlaying(true);
+    }
+
+    useEffect(()=>{
+        setTimeout(()=>{
+           audioRef.current.ontimeupdate=()=>{
+            seekBar.current.style.width =(Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%";
+            setTime({
+                currentTime:{
+                    second: Math.floor(audioRef.current.currentTime % 60),
+                    minute: Math.floor(audioRef.current.currentTime / 60)
+                },
+                totalTime:{
+                    second: Math.floor(audioRef.current.duration % 60),
+                    minute: Math.floor(audioRef.current.duration / 60)
+                }
+            })
+           }
+        },1000);
+    },{audioRef}) 
+
     const contextValue = {
         audioRef,
         seekBar,
@@ -39,7 +63,8 @@ const PlayerContextProvider = (props)=>{
         track,setTrack,
         playing,setPlaying,
         time,setTime,
-        play,pause
+        play,pause,
+        playWithId
     }
 
     return(
