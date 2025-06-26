1dd7fe6f0ec132c742c18f0b439ec39ab8912ec6
MRMSANJAI <mrmsanjai@gmail.com>
2025-06-26 21:11:11 +0530

    260525

--- BACKEND/src/routes/songRoute.js
+++ BACKEND/src/routes/songRoute.js
@@ -1,9 +1,17 @@
-import {listSong, addSong } from "../Controllers/songController.js";
-import express from "express"
+import express from "express";
+import { listSong, addSong } from "../Controllers/songController.js";
+import upload from "../middleware/multer.js";
 
 const songRouter = express.Router();
 
-songRouter.post('/add', addSong);
-songRouter.get('/list', listSong);
+songRouter.post("/add",upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "audio", maxCount: 1 }
+  ]),
+  addSong
+);
+
+
+songRouter.get("/list", listSong);
 
 export default songRouter;
